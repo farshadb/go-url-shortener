8a0406bb40d7ee8c9f016c800a47a67322fd3947
F4r5h4d <bolouri.farshad@gmail.com>
2022-07-31 21:57:35 +0430

    ResolveURL function created.

---
+++ api/routes/resolve.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"go-url-shortener/database"
+
+	"github.com/F4r5h4d/go-url-shortener/database"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err: = r.Get(database.Ctx, url).Result()
+
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{ "error": 
+		"short not found in database" 
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			 "error": "Can't connect to DB" 
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, user)
+
+
+}
